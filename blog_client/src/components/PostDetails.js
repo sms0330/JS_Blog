00e30bf642a383ceb4315315dd9d542f0400e2fa
Joseph Son <sms0330@gmail.com>
2021-09-03 23:25:19 -0700

    Fixed styling

--- blog_client/src/components/PostDetails.js
+++ blog_client/src/components/PostDetails.js
@@ -4,14 +4,14 @@ function PostDetails(props) {
   let [ like, setLike ] = useState(0)
 
   return (
-    <div>
-      <h2>Title: {props.title}</h2>
+    <div className="ui segment">
+      <span className="ui teal tag label">{props.tags.map(tag => tag.name).join(' ')}</span>
+      <h2 >Title: {props.title}</h2>
       <div className="meta">
         <i className="like">
           <i className="like pink icon" onClick={ () => setLike(like + 1) }></i> {props.favourites_count+like} Likes
         </i>
       </div>
-      <h4 className="ui teal tag label">{props.tags.map(tag => tag.name).join(' ')}</h4>
       <img src={`http://localhost:3000${props.image.url}`} alt={props.title}/>
       <p>
         Body:
